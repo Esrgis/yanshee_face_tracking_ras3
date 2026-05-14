4475d432cb13d25db887ec9dd9c2d05f751194ff
Esrgis <nthang251@gmail.com>
2026-05-14 20:09:52 +0700

    s

--- hardware/yanshee_servo_test.py
+++ hardware/yanshee_servo_test.py
@@ -11,8 +11,8 @@ def main():
     ap = argparse.ArgumentParser()
     ap.add_argument("--ip", default="127.0.0.1")
     ap.add_argument("--servo", default="NeckLR")
-    ap.add_argument("--duration", type=int, default=800)
-    ap.add_argument("--angles", default="90,70,110,90")
+    ap.add_argument("--duration", type=int, default=1200)
+    ap.add_argument("--angles", default="90,45,135,90")
     ap.add_argument("--list-api", action="store_true")
     args = ap.parse_args()
 
@@ -25,15 +25,26 @@ def main():
     init_resp = YanAPI.yan_api_init(args.ip)
     print("[TEST] yan_api_init({}) -> {}".format(args.ip, init_resp))
 
+    prev_angle = None
     for raw in args.angles.split(","):
         angle = int(raw.strip())
-        print("[TEST] set {}={} duration={}".format(args.servo, angle, args.duration))
+        if prev_angle is None:
+            direction = "center/start"
+        elif angle < prev_angle:
+            direction = "angle decreasing"
+        elif angle > prev_angle:
+            direction = "angle increasing"
+        else:
+            direction = "same angle"
+        print("[TEST] set {}={} duration={} ({})".format(
+            args.servo, angle, args.duration, direction))
         try:
             resp = YanAPI.set_servos_angles({args.servo: angle}, args.duration)
             print("[TEST] response -> {}".format(resp))
         except Exception as e:
             print("[TEST] exception -> {}".format(e))
         time.sleep(max(0.3, args.duration / 1000.0 + 0.2))
+        prev_angle = angle
 
 
 if __name__ == "__main__":
